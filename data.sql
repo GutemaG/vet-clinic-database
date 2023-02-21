4a12bc6ae09199cdb74587997c9e71fb5a5369dd
GutemaG <bir13gud@gmail.com>
2023-02-21 20:34:04 +0300

    Create vet_clinic database and animals table and fill the table with some values and add SQl Quries

--- data.sql
+++ data.sql
@@ -1,5 +1,19 @@
 /* Populate database with sample data. */
 
-INSERT INTO animals (name) VALUES ('Luna');
-INSERT INTO animals (name) VALUES ('Daisy');
-INSERT INTO animals (name) VALUES ('Charlie');
+INSERT INTO animals(
+	id, name, date_of_birth, escape_attempts, neutered, weight_kg)
+	values( 1, 'Agumon', 'Feb 3, 2020', 0, true, 10.23);
+
+INSERT INTO animals(
+	id, name, date_of_birth, escape_attempts, neutered, weight_kg)
+	values( 2, 'Gabumon', 'Nov 15, 2018', 2, true, 8);
+	
+
+INSERT INTO animals(
+	id, name, date_of_birth, escape_attempts, neutered, weight_kg)
+	values( 3, 'Pikachu', 'Jan 7, 2021', 1, false, 15.04);
+
+INSERT INTO animals(
+	id, name, date_of_birth, escape_attempts, neutered, weight_kg)
+	values( 4, 'Devimon', 'May 12, 2017', 5, true, 15);
+	
